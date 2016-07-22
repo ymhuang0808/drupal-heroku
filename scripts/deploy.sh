e1d61afc7907a8f2591118a7a8ec9bb70e773aa4
YMHuang <ym.huang0808@gmail.com>
2016-07-22 15:46:16 +0800

    After composer creating project, execute composer install in drupal/

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,7 @@ drupal_path="$path/drupal"
 ## create Drupal 8.1 latest
 composer create-project drupal/drupal:~8.1 drupal
 
+cd "$drupal_path" && composer install
+
 ## configure
 cp "$path/conf/settings.php" "$drupal_path/sites/default/"
