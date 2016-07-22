d6473b0be91edea1045c782b9cb6c67d2547cf23
YMHuang <ym.huang0808@gmail.com>
2016-07-22 15:21:24 +0800

    Remove recursive git

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,6 @@ drupal_version="8.1.7"
 
 drupal_path="$path/drupal"
 
-## clone submodule
-git submodule update --init --recursive
-
 ## build drupal
 cd "$drupal_path" && git checkout --force $drupal_version && composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
 
