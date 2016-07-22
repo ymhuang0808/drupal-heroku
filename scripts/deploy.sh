61f24b455221a00ad69e24eb34272b078322fd18
YMHuang <ym.huang0808@gmail.com>
2016-07-22 15:31:39 +0800

    Remove git checkouy in scripts/deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ drupal_version="8.1.7"
 drupal_path="$path/drupal"
 
 ## build drupal
-cd "$drupal_path" && git checkout --force $drupal_version && composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
+cd "$drupal_path" && composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
 
 ## configure
 cp "$path/conf/settings.php" "$drupal_path/sites/default/"
