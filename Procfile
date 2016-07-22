b871cf578b1684805e996a7cf2280ebe7940c045
YMHuang <ym.huang0808@gmail.com>
2016-07-22 16:09:52 +0800

    Change composer to create project into download tar

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-nginx drupal
+web: vendor/bin/heroku-php-nginx drupal/
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,15 @@ path=`pwd`
 
 drupal_path="$path/drupal"
 
-## create Drupal 8.1 latest
-composer create-project drupal/drupal:~8.1 drupal
+DRUPAL_VERSION=8.1.7
 
-cd "$drupal_path" && composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
+## create Drupal 8.1 latest
+mkdir $drupal_path && cd $drupal_path
+curl -fSL "http://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz" -o drupal.tar.gz \
+	&& echo "${DRUPAL_MD5} *drupal.tar.gz" | md5sum -c - \
+	&& tar -xz --strip-components=1 -f drupal.tar.gz \
+	&& rm drupal.tar.gz \
+  && chmod 755 sites
 
 ## configure
 cp "$path/conf/settings.php" "$drupal_path/sites/default/"
