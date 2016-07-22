cfec26eead90c98191935e0665ab8f35535cbd57
YMHuang <ym.huang0808@gmail.com>
2016-07-22 15:57:18 +0800

    update composer.lock

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ drupal_path="$path/drupal"
 ## create Drupal 8.1 latest
 composer create-project drupal/drupal:~8.1 drupal
 
-cd "$drupal_path" && composer install
+cd "$drupal_path" && composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
 
 ## configure
 cp "$path/conf/settings.php" "$drupal_path/sites/default/"
