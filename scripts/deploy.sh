109e8729996705b4002b9affe2fa5b5a9a02c70b
YMHuang <ym.huang0808@gmail.com>
2016-07-22 15:34:35 +0800

    Drupal is created by composer

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,11 @@
 echo 'Run scripts/deploy.sh'
 
 path=`pwd`
-drupal_version="8.1.7"
 
 drupal_path="$path/drupal"
 
-## build drupal
-cd "$drupal_path" && composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
+## create Drupal 8.1 latest
+composer create-project drupal/drupal:~8.1 drupal
 
 ## configure
 cp "$path/conf/settings.php" "$drupal_path/sites/default/"
