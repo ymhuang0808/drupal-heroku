b6293d52d137199885c951bbd20f2e611dab075e
YMHuang <ym.huang0808@gmail.com>
2016-07-22 15:15:07 +0800

    Fix for heroku

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,11 @@ drupal_version="8.1.7"
 
 drupal_path="$path/drupal"
 
+## clone submodule
+git submodule update --init --recursive
+
 ## build drupal
 cd "$drupal_path" && git checkout --force $drupal_version && composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
 
+## configure
 cp "$path/conf/settings.php" "$drupal_path/sites/default/"
